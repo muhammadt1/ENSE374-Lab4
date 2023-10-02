fa0a6d8a9b176debb038bf3678fd1c08bbbd0a05
Muhammad Tariq <muhammad.etariq@gmail.com>
2023-10-01 18:05:53 -0600

    redid js, added json, made changes to index & css

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-var  mainArray = [];
+var arr = [];
+var l;
 
-function addedDivSec()
+function makeList()
 {
-    let d = document.createElement("div");
-    d.className = "input-group mb-3";
+    
+    l = document.getElementById("added");
+    let inp = document.getElementById("task").value;
+    
+    arr.push(inp);
+    l.innerHTML += '<div id="ent"><li class="list-group-item">' + inp + "</li> </div>";
+    
+}
 
+function sortList()
+{
+    
+    arr.sort();
+    l.innerHTML = "";
+    
+    for( var i = 0; i < arr.length; i++)
+    {
+        l.innerHTML += '<div id="ent"><li class="list-group-item">' + arr[i] + "</li> </div>";
+    }
 }
